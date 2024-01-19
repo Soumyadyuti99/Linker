8ecb209f01f85bc56acbda3f7401c05264c43b5d
Soumyadyuti Nandy <soumyadyutinandy1999@gmail.com>
2024-01-19 19:46:17 +0530

    fixed oauth

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,13 +1,11 @@
 import React from "react";
-import { NavLink, Outlet } from "react-router-dom";
-import { useAuth0 } from "@auth0/auth0-react";
+import { NavLink, Outlet, Link } from "react-router-dom";
+
 
 export default function Layout() {
-    const { loginWithRedirect, isAuthenticated, user} = useAuth0();
-    const { logout } = useAuth0();
+    
     const url = 'http://localhost:3001/login/api';
-    const data =  user;
-    const requestOptions = {
+    /*const requestOptions = {
         method: 'POST',
         headers: {
         'Content-Type': 'application/json', 
@@ -31,18 +29,15 @@ export default function Layout() {
             }
         }
         PostUser();
-    }, [user])
+    }, [user])*/
     
     
     return(
         <div>
             <nav>
                 <NavLink className='links' to='/'>#LINKER</NavLink>
-                <div className="info">
-                    {!isAuthenticated && <button className="login-out-btn" onClick={
-                        ()=>loginWithRedirect() }>
-                    Login/Sign Up</button>}
-                    {isAuthenticated && <button className="login-out-btn" onClick={()=> logout()}>Logout</button>}
+                <div className="info">                    
+                    <Link to='/login' className="login-out-btn">Login</Link>
                     <NavLink className='links' to='/user'>User</NavLink>
                 </div>                
             </nav>
